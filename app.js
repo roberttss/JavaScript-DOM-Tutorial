0b0358fb93f639b4fa6277d7c5ac0d30ccda7080
roberttss <robertabolins772@gmail.com>
2021-03-09 16:36:36 +0200

    komit8

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const bookList = document.querySelector('#book-list')
 
-console.log('the parent node is:', bookList.parentNode);
-console.log('the parent element is:', bookList.parentElement.parentElement);
+console.log('book-list next sibiling is:',bookList.nextSibling);
+console.log('book-list next element sibiling is:', bookList.nextElementSibling);
 
-console.log(bookList.children);
+console.log('book-list previous sibiling is:',bookList.previousSibling);
+console.log('book-list previous element sibiling is:', bookList.previousElementSibling);
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br/>Too cool for everyone else!<br>'
