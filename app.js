b74cafd4fa9783b979a6c67f9dd562e4af01ec99
roberttss <robertabolins772@gmail.com>
2021-03-09 16:12:00 +0200

    komit6

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-var bboks = Document.querySelectorAll('#book-list li .name');
+conset banner = document.querySelector('#page-banner');
 
-Array.from(books).forEach(function(book){
-    book.textContent +='(book title)';
-});
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
 
-const bookList = document.querySelector('#book-list');
-//bookList.innerHTML = '<h2>Books and more books...</h2>';
-bookList.innerHTML += '<p>This is how you add HTML</p>';
+cost cloneBanner = banner.cloneNode(true);
+console.log(clonedBanner);
