8f3d20e2072844dd43b65b2b4f496bf51122f367
roberttss <robertabolins772@gmail.com>
2021-03-16 17:00:57 +0200

    komit ?

--- app.js
+++ app.js
@@ -2,32 +2,11 @@ const list = document.querySelector('#book-list ul');
 
 //delete books
 list.addEventListener('click', function(e){
-    if(e.target.className == 'delete'){
-        const li = e.target.parentElement;
-        list.removeChild(li);
+ // add books
+ conast addForm = document.forms['add-book'];
+ addForm.addEventListener('submit',function(e){
+     
+ })
     }
 });
 
-// add boks-list
-const addForm = document.forms['add-book'];
-addForm.addEventListener('submit', function(e){
-    e.preventDefault();
-    const value = addForm.querySelector('input[type="text"]').value;
-
-    //create elements 
-    const li = document.createElement('li');
-    const bookName = document.createElement('span');
-    const deleteBtn = document.createElement('span');
-
-    //add content
-    deleteBtn.textContent = 'delete';
-    bookName.textContent = 'value';
-
-    //add clasess
-    bookName.classList.add('name');
-    deleteBtn.classList.add('delete');
-
-    //append to document 
-    li.appendChild(bookName);
-    li.appendChild(deleteBtn);
-    list.appendChild(li);
