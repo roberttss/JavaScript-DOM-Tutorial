0791cb1528467b45f8126af3e190b7f86490e9d4
roberttss <robertabolins772@gmail.com>
2021-03-16 15:10:46 +0200

    komit 11

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const list = document.querySelector('#book-list ul');
 
-
+//delete books
 list.addEventListener('click', function(e){
     if(e.target.className == 'delete'){
         const li = e.target.parentElement;
         list.removeChild(li);
     }
+});
+
+// add boks-list
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
 });
